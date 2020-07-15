c9a17f5d68b8c851ccaf8bf20e722a07f740b6e1
girlsops <qilinpublic@outlook.com>
2020-07-15 15:28:18 +0800

    course 3.01

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import _ from "lodash";
 
 import { name as o } from "./index2.js";
+import printMe from "./print";
 
 document.writeln(o);
 document.writeln("<br>");
 document.writeln(_.max(["1", "2", "3", "11", "22"]));
+printMe();
