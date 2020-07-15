7d1496da9d934d0f2c67e7ff822d949033e6000e
girlsops <qilinpublic@outlook.com>
2020-07-15 15:43:54 +0800

    course 3.03

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: "./index.js",
         print: "./print.js"
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
