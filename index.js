d1bca0186e7136eea426a08570b3517d18c444c0
girlsops <qilinpublic@outlook.com>
2020-07-15 14:46:17 +0800

    course 2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import "./CSS/style.css";
+import img1 from "./IMG/img1.jpg";
+import data1 from "./DATA/data.xml";
+
 import _ from "lodash";
 
 import { name as o } from "./index2.js";
@@ -5,3 +9,6 @@ import { name as o } from "./index2.js";
 document.writeln(o);
 document.writeln("<br>");
 document.writeln(_.max(["1", "2", "3", "11", "22"]));
+document.writeln(`<br>${img1}`);
+document.writeln(`<br><img src=dist/${img1}`);
+console.log(data1);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,25 @@ module.exports = {
     entry: "./index.js",
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname)
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,             // 需要import
+            use: ["style-loader"        // css文件将被加入html的head中
+                , "css-loader"]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ["file-loader"]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ['file-loader']
+        }, {
+            test: /\.(csv|tsv)$/,
+            use: ['csv-loader']
+        }, {
+            test: /\.xml$/,
+            use: ['xml-loader']
+        }]
     }
 };
