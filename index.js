5a7ddf12a94bc037ebd0683dbb213db7feab9a6f
girlsops <qilinpublic@outlook.com>
2020-07-15 15:24:26 +0800

    new course 1

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-import "./CSS/style.css";
-import img1 from "./IMG/img1.jpg";
-import data1 from "./DATA/data.xml";
-
 import _ from "lodash";
 
 import { name as o } from "./index2.js";
@@ -9,6 +5,3 @@ import { name as o } from "./index2.js";
 document.writeln(o);
 document.writeln("<br>");
 document.writeln(_.max(["1", "2", "3", "11", "22"]));
-document.writeln(`<br>${img1}`);
-document.writeln(`<br><img src=dist/${img1}`);
-console.log(data1);
